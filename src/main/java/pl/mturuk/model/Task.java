38499b1680eff5930f42901e999dafe0a40b592a
MirekTuruk <miroslaw.turuk@gmail.com>
2019-08-06 12:01:05 +0200

    add

---
+++ src/main/java/pl/mturuk/model/Task.java
@@ -0,0 +1,6 @@
+package pl.mturuk.model;
+
+
+public class Task {
+
+}
---
+++ src/main/java/pl/mturuk/service/ToDoListChooserStrategy.java
@@ -0,0 +1,14 @@
+package pl.mturuk.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.mturuk.model.Task;
+
+public class ToDoListChooserStrategy {
+
+	
+	public List<Task> createToDoList (){
+		return new ArrayList<Task>();
+	}
+}
